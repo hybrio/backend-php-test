a4f05987a74ac3d8ae9d8273c934b85fae683eec
Alexander Maehl <amaehl@inmusicbrands.com>
2021-12-19 16:14:08 +1300

    [Task 3] users can retrieve todos in JSON format

--- src/controllers.php
+++ src/controllers.php
@@ -65,6 +65,21 @@ $app->get('/todo/{id}', function ($id) use ($app) {
 ->value('id', null);
 
 
+$app->get('/todo/{id}/json', function ($id) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect('/login');
+    }
+
+    if ($id){
+        $sql = "SELECT * FROM todos WHERE id = '$id'";
+        $todo = $app['db']->fetchAssoc($sql);
+
+        return json_encode($todo);
+    }
+})
+->value('id', null);
+
+
 $app->post('/todo/add', function (Request $request) use ($app) {
     if (null === $user = $app['session']->get('user')) {
         return $app->redirect('/login');
